54a427adf3e103b0d3b6b4baf7c8923e897ea8b9
jsigm <jsigman04@gmail.com>
2021-06-28 21:45:50 -0400

    PA1 - File reading/writing

--- ProgrammingAssignment1/spellcheck.cpp
+++ ProgrammingAssignment1/spellcheck.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+string getFileName() {
+    string fileDir;
+    cout << " file name: ";
+    getline(cin,fileDir);
+    return fileDir;
+}
+
+void readDictionary(ifstream& dictFile) {
+    string read;
+
+    while (getline(dictFile, read)) {
+        //cout << "PROCESSED: " << read << "\n";
+    }
+
+    dictFile.close();
+}
+
+void readFile(ifstream& inFile, ofstream& outFile) {
+    string read;
+
+    /** Main loop for reading each line of the input file.
+     */
+
+    while (getline(inFile, read)) {
+        //cout << "PROCESSED: " << read << "\n";
+    }
+
+    inFile.close();
+    outFile.close();
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    ofstream outFile;
+    ifstream inFile;
+    ifstream dictFile;
+
+    cout << "Output";
+    outFile.open(getFileName());
+    cout << "Input";
+    inFile.open(getFileName());
+    cout << "Dictionary";
+    dictFile.open(getFileName());
+
+    readDictionary(dictFile);
+    readFile(inFile, outFile);
 }
