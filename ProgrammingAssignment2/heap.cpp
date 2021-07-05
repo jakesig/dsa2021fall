1500614eb2fe738ad6f6cf4f75c731d4f3983759
jsigm <jsigman04@gmail.com>
2021-07-05 18:18:26 -0400

    PA2 - Getting Heap Started

--- ProgrammingAssignment2/heap.cpp
+++ ProgrammingAssignment2/heap.cpp
@@ -1,4 +1,48 @@
-//
-// Created by jsigm on 6/27/2021.
-//
+#include <string>
+#include <vector>
+#include "heap.h"
 
+heap::heap(int capacity):mapping(capacity*2) {
+    data.resize(capacity+1);
+    this -> capacity = capacity;
+}
+
+int heap::insert(const std::string &id, int key, void *pv) {
+    if (filled == capacity)
+        return 1;
+
+    if (mapping.contains(id))
+        return 2;
+
+    data[++filled].id = id;
+    data[filled].key = key;
+    data[filled].pData = pv;
+
+    mapping.insert(id, &data[filled]);
+
+    return 0;
+}
+
+int heap::setKey(const std::string &id, int key) {
+
+}
+
+int heap::remove(const std::string &id, int *pKey, void *ppData) {
+
+}
+
+int heap::deleteMin(std::string *pId, int *pKey, void *ppData) {
+
+}
+
+void heap::percolateUp(int posCur) {
+
+}
+
+void heap::percolateDown(int posCur) {
+
+}
+
+int heap::getPos(node *pn) {
+    return pn - &data[0];
+}
