1014bcf1bc82f7bf2258ffb1649558ac0aea3ef9
jsigm <jsigman04@gmail.com>
2021-07-06 00:26:35 -0400

    PA2 - Began comment refactoring

--- ProgrammingAssignment2/heap.h
+++ ProgrammingAssignment2/heap.h
@@ -5,80 +5,87 @@
 class heap {
 
     public:
-        //
-        // heap - The constructor allocates space for the nodes of the heap
-        // and the mapping (hash table) based on the specified capacity
-        //
+
+        /** heap(): Initializes heap, allocating space for the nodes of heap,
+         *  and the mapping (hash table) based on the specified capacity.
+         *
+         *  @param {int} capacity - The initial capacity of the heap.
+         */
+
         heap(int capacity);
 
-        //
-        // insert - Inserts a new node into the binary heap
-        //
-        // Inserts a node with the specified id string, key,
-        // and optionally a pointer.  They key is used to
-        // determine the final position of the new node.
-        //
-        // Returns:
-        //   0 on success
-        //   1 if the heap is already filled to capacity
-        //   2 if a node with the given id already exists (but the heap
-        //     is not filled to capacity)
-        //
+        /** insert(): Inserts a new node with the specified id, string,
+         *  key, and an optional pointer into the binary heap. The key
+         *  is used to determine the final position of the new node.
+         *
+         *  @param {string} id - ID to insert onto heap.
+         *  @param {int} key - Key to insert onto heap.
+         *  @param {void *} pv - Optional pointer to insert onto heap.
+         *  @return {int} - 0 on success, 1 if at capacity, 2 if a node with the given id already exists.
+         */
+
         int insert(const std::string &id, int key, void *pv = nullptr);
 
-        //
-        // setKey - set the key of the specified node to the specified value
-        //
-        // I have decided that the class should provide this member function
-        // instead of two separate increaseKey and decreaseKey functions.
-        //
-        // Returns:
-        //   0 on success
-        //   1 if a node with the given id does not exist
-        //
+        /** setKey(): Sets the key of the specified node to the specified value.
+         *
+         *  @param {string} id - The ID of the node.
+         *  @param {int} key - The key to set.
+         *  @return {int} - 0 on success, 1 if node with given id does not exist.
+         */
+
         int setKey(const std::string &id, int key);
 
-        //
-        // deleteMin - return the data associated with the smallest key
-        //             and delete that node from the binary heap
-        //
-        // If pId is supplied (i.e., it is not nullptr), write to that address
-        // the id of the node being deleted. If pKey is supplied, write to
-        // that address the key of the node being deleted. If ppData is
-        // supplied, write to that address the associated void pointer.
-        //
-        // Returns:
-        //   0 on success
-        //   1 if the heap is empty
-        //
+        /** deleteMin(): Returns the data associated with the smallest key, and deletes that node from the heap.
+         *
+         *  @param {string *} pId - If supplied, write to that address the ID of the deleted node.
+         *  @param {int *} pKey - If supplied, write to that address the key of the deleted node.
+         *  @param {void *} ppData - If supplied, write to that address the associated void pointer.
+         *  @return {int} - 0 on success, 1 if the heap is empty.
+         */
+
         int deleteMin(std::string *pId = nullptr, int *pKey = nullptr, void *ppData = nullptr);
 
-        //
-        // remove - delete the node with the specified id from the binary heap
-        //
-        // If pKey is supplied, write to that address the key of the node
-        // being deleted. If ppData is supplied, write to that address the
-        // associated void pointer.
-        //
-        // Returns:
-        //   0 on success
-        //   1 if a node with the given id does not exist
-        //
+        /** remove(): Deletes the node with the specified id from the binary heap.
+         *
+         *  @param {string} id - The id of the node to be removed.
+         *  @param {int *} pKey - If supplied, write to that address the key of the deleted node.
+         *  @param {void *} ppData - If supplied, write to that address the associated void pointer.
+         *  @return {int} - 0 on success, 1 if the node with the given id does not exist.
+         */
+
         int remove(const std::string &id, int *pKey = nullptr, void *ppData = nullptr);
 
     private:
+
+        /** node subclass
+         */
+
         class node {
+
+            /** Public variables for hashItem:
+             *  id - the ID of the node.
+             *  key - int used as a key.
+             *  pData - a pointer related to the node; nullptr if no key provided.
+             */
+
             public:
                 std::string id;
                 int key;
                 void *pData;
+
         };
 
-        int capacity; // The current capacity of the hash table.
-        int filled{}; // Number of occupied items in the table.
+        /** Private variables for heap:
+         *  capacity - Contains the current capacity of the heap.
+         *  filled - Contains the number of occupied items in the heap.
+         *  data - The actual binary heap.
+         *  mapping - maps ids to node pointers.
+         */
 
-        std::vector<node> data; // The actual binary heap
-        hashTable mapping; // maps ids to node pointers
+        int capacity;
+        int filled{};
+        std::vector<node> data;
+        hashTable mapping;
 
         void percolateUp(int posCur);
         void percolateDown(int posCur);
