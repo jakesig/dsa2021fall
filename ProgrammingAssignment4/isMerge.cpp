8ab50693cb13f38851147d0d8416dad00f9f0550
jsigm <jsigman04@gmail.com>
2021-07-20 22:40:48 -0400

    PA4 - Dynamic working, output stuff pending

--- ProgrammingAssignment4/isMerge.cpp
+++ ProgrammingAssignment4/isMerge.cpp
@@ -15,6 +15,15 @@ using namespace std;
 
 bool matrix[1000][1000];
 
+/** isMergeRecursive(): Determines if a string is a merge of two other strings recursively.
+ *  This method does has exponential time complexity and therefore won't be effective.
+ *
+ *  @param {string} str1 - First string being merged.
+ *  @param {string} str2 - Second string being merged.
+ *  @param {string} str3 - The string to check if it's a merge of the first two strings.
+ *  @return {bool} - Whether or not the third string is a merge of the first two strings.
+ */
+
 bool isMergeRecursive(const string& str1, const string& str2, const string& str3) {
 
     /** Base case: If all the strings are empty return true.
@@ -52,6 +61,13 @@ bool isMergeRecursive(const string& str1, const string& str2, const string& str3
     return str1Match || str2Match;
 }
 
+/** isMergeDynamic(): Determines if a string is a merge of two other strings using dynamic programming.
+ *
+ *  @param {string} str1 - First string being merged.
+ *  @param {string} str2 - Second string being merged.
+ *  @param {string} str3 - The string to check if it's a merge of the first two strings.
+ *  @return {bool} - Whether or not the third string is a merge of the first two strings.
+ */
 
 bool isMergeDynamic(const string& str1, const string& str2, const string& str3) {
 
@@ -62,15 +78,16 @@ bool isMergeDynamic(const string& str1, const string& str2, const string& str3) 
     for (int i = 0;  i < 1000; ++i) {
         for (int j = 0; j < 1000; ++j) {
 
-            /** Case 1: If both strings are empty, set slot to true.
-              */
+            /** Case 1: If both strings are empty, set cell to true.
+             *  Similar to the base case of the recursive solution.
+             */
 
             if (i == 0 && j == 0)
                 matrix[i][j] = true;
 
             /** Case 2: If str1 and str2 are not empty, and the current characters of
-             *  str1, str2, and str3 are the same, set slot to either the slot to the left,
-             *  or the slot below. This is equivalent to the final line of the recursive
+             *  str1, str2, and str3 are the same, set the cell to either the cell on the left,
+             *  or the cell below. This is equivalent to the final line of the recursive
              *  solution, which is intended for handling any duplicate characters amongst
              *  the three strings.
              */
@@ -79,7 +96,7 @@ bool isMergeDynamic(const string& str1, const string& str2, const string& str3) 
                 matrix[i][j] = matrix[i][j - 1] || matrix[i - 1][j];
 
             /** Case 3: If str1 is not empty, and the current characters of
-             *  str1 and str3 are the same, set slot to the slot to the left.
+             *  str1 and str3 are the same, set the cell to the cell on the left.
              *  This is equivalent to removing one character in the recursive
              *  solution above.
              */
@@ -88,16 +105,26 @@ bool isMergeDynamic(const string& str1, const string& str2, const string& str3) 
                 matrix[i][j] = matrix[i - 1][j];
 
             /** Case 4: If str2 is not empty, and the current characters of
-             *  str2 and str3 are the same, set slot to the slot below.
+             *  str2 and str3 are the same, set the cell to the cell below.
              *  This is equivalent to removing one character in the recursive
              *  solution above.
              */
 
             else if (j != 0 && str2[j - 1] == str3[(i + j) - 1])
                 matrix[i][j] = matrix[i][j - 1];
-
+            
+            /** If the code ever makes it here, just set the cell to false
+             *  if it's not false already.
+             */
+            
+            else
+                matrix[i][j] = false;
         }
     }
+
+    /** Simply access the value in the matrix at the size of str1 and str2 for
+     *  the row and column respectively.
+     */
 
     return matrix[str1.size()][str2.size()];
 }
--- ProgrammingAssignment4/isMerge.h
+++ ProgrammingAssignment4/isMerge.h
@@ -14,7 +14,24 @@
 
 #include <string>
 
+/** isMergeRecursive(): Determines if a string is a merge of two other strings recursively.
+ *  This method does has exponential time complexity and therefore won't be effective.
+ *
+ *  @param {string} str1 - First string being merged.
+ *  @param {string} str2 - Second string being merged.
+ *  @param {string} str3 - The string to check if it's a merge of the first two strings.
+ *  @return {bool} - Whether or not the third string is a merge of the first two strings.
+ */
+
 bool isMergeRecursive(const std::string& str1, const std::string& str2, const std::string& str3);
+
+/** isMergeDynamic(): Determines if a string is a merge of two other strings using dynamic programming.
+ *
+ *  @param {string} str1 - First string being merged.
+ *  @param {string} str2 - Second string being merged.
+ *  @param {string} str3 - The string to check if it's a merge of the first two strings.
+ *  @return {bool} - Whether or not the third string is a merge of the first two strings.
+ */
 
 bool isMergeDynamic(const std::string& str1, const std::string& str2, const std::string& str3);
 
